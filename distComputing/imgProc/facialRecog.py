95db817f68e31b3b53a0353294959e1e97830210
szwlgtc <>
2020-12-31 01:20:39 +0800

    change experiment

--- distComputing/imgProc/facialRecog.py
+++ distComputing/imgProc/facialRecog.py
@@ -18,7 +18,7 @@ def facial_recog(image_path):
     image = face_recognition.load_image_file(image_path, mode='L')
 
     start_time = int(round(time.time() * 1000))
-    res = len(face_recognition.face_locations(image, number_of_times_to_upsample=3, model='hog'))
+    res = len(face_recognition.face_locations(image, number_of_times_to_upsample=4, model='hog'))
     end_time = int(round(time.time() * 1000))
     time_cost = end_time - start_time
     # print("face_locations cost time(/ms): ", time_cost)
